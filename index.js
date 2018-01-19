5d456851855c4da18a36d13f1f2bc32b4a8a7a1e
calvinzheng <calvinzheng@exosite.com>
2018-01-20 00:16:25 +0800

    Init

---
+++ index.js
@@ -0,0 +1,40 @@
+import { createStore, combineReducers } from './redux';
+
+// action
+let addBike = (bike) => {
+    return {
+        type: 'add_bike',
+        bike
+    }
+}
+let removeBike = (bike) => {
+    return {
+        type: 'remove_bike',
+        bike
+    }
+}
+// reducer
+let bike = (state = [], action) => {
+    switch (action.type) {
+        case 'add_bike':
+            return [...state, action.bike]
+        case 'remove_bike':
+            return state.filter(bike => action.bike.name !== bike.name)
+        default:
+            return state
+    }
+
+}
+let reducer = (state = {}, action) => {
+    return {
+        bike: bike(state.bike, action)
+    }
+}
+
+let store = createStore(reducer)
+const unsubscribe = store.subscribe(() =>
+    console.log(store.getState())
+)
+store.dispatch(addBike({ name: 'giant' }))
+store.dispatch(addBike({ name: 'trek' }))
+store.dispatch(removeBike({ name: 'giant' }))
